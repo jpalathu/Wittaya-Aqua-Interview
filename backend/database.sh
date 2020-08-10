8ba4dc1f35b740be12fcb665249cbd1c0c42d3a5
jpalathu <jpalathu@uwaterloo.ca>
2020-08-10 13:06:15 -0400

    fixed connection and port mapping to connect to db

--- backend/database.sh
+++ backend/database.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 npx knex migrate:rollback
 npx knex migrate:latest
 npx knex seed:run --specific="test.js"
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: "localhost",
-      user: "postgres",
-      password: "FishDev123",
-      database: "test_database",
-      port: 5432,
+      host: process.DB_HOST || "postgres",
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "FishDev123",
+      database: process.env.DB_NAME || "test_database",
+      port: process.env.DB_PORT || "5432",
     },
     migrations: { directory: "database/migrations" },
     seeds: { directory: "database/seeds" },
